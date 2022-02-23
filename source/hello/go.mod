544ef72a35d401ab54661e6550f5a15ee6cc0455
Mucheru <mucheru@outlook.com>
2022-02-23 18:21:05 +0300

    Code cleanup

---
+++ source/hello/go.mod
@@ -0,0 +1,8 @@
+module github.com/Muchezz/go-lambda-terraform-setup
+
+go 1.17
+
+require (
+	github.com/aws/aws-lambda-go v1.28.0
+	github.com/muchezz/go-lambda-terraform-setup v0.0.0-20220223142519-65eb97eddd6e
+)
--- source/hello/main.go
+++ source/hello/main.go
@@ -4,7 +4,7 @@ package main
 
 import (
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/muchezz/go-lambda-terraform-setup/source/hello"
+	"github.com/muchezz/go-lambda-terraform-setup/source/hello/handler"
 )
 
 func main() {
